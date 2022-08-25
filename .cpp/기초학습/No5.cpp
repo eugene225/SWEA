47ab6e4352442c95a174ccac6cc3e796e5f6ee74
eugene225 <81062608+eugene225@users.noreply.github.com>
2022-08-25 23:13:30 +0900

    No5 update

--- .cpp/기초학습/No5.cpp
+++ .cpp/기초학습/No5.cpp
@@ -1,122 +1,214 @@
-#include <iostream>
-using namespace std;
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+#define MAX_NODE 10000
+
+enum { ADD_HEAD = 1, ADD_TAIL, ADD_NUM, FIND, REMOVE, PRINT, PRINT_REVERSE, END = 99 };
+
+extern void init();
+extern void addNode2Head(int data);
+extern void addNode2Tail(int data);
+extern void addNode2Num(int data, int num);
+extern int findNode(int data);
+extern void removeNode(int data);
+extern int getList(int output[MAX_NODE]);
+extern int getReversedList(int output[MAX_NODE]);
+
+static void run() {
+	while (1) {
+		int cmd, data, num, cnt, i = 0;
+		int output[MAX_NODE] = { 0 };
+
+		scanf("%d", &cmd);
+		switch (cmd) {
+		case ADD_HEAD: // 1
+			scanf("%d", &data);
+			addNode2Head(data);
+			break;
+		case ADD_TAIL: // 2
+			scanf("%d", &data);
+			addNode2Tail(data);
+			break;
+		case ADD_NUM: // 3
+			scanf("%d %d", &data, &num);
+			addNode2Num(data, num);
+			break;
+		case FIND: // 4
+			scanf("%d", &data);
+			num = findNode(data);
+			printf("%d\n", num);
+			break;
+		case REMOVE: // 5
+			scanf("%d", &data);
+			removeNode(data);
+			break;
+		case PRINT: // 6
+			cnt = getList(output);
+			while (cnt--)
+				printf("%d ", output[i++]);
+			printf("\n");
+			break;
+		case PRINT_REVERSE: // 7
+			cnt = getReversedList(output);
+			while (cnt--)
+				printf("%d ", output[i++]);
+			printf("\n");
+			break;
+		case END:
+			return;
+		}
+	}
+}
+
+int main(void) {
+	//setbuf(stdout, NULL);
+	//freopen("dll_input.txt", "r", stdin);
+
+	int T;
+	scanf("%d", &T);
+	for (int t = 1; t <= T; t++) {
+		printf("#%d\n", t);
+		init();
+		run();
+		printf("\n");
+	}
+
+	return 0;
+}
+
+#define MAX_NODE 10000
 
 struct Node {
 	int data;
-	Node* Llink;
-	Node* Rlink;
+	Node* prev;
+	Node* next;
 };
 
-struct HeadNode {
-	Node* Lhead; // 원래 리스트의 반대쪽으로
-	Node* Rhead; // 원래 연결리스트 방향으로
-};
+Node node[MAX_NODE];
+int nodeCnt;
+Node* head;
+Node* tail;
 
-class doubleList {
+Node* getNode(int data) {
+	node[nodeCnt].data = data;
+	node[nodeCnt].prev = nullptr;
+	node[nodeCnt].next = nullptr;
+	return &node[nodeCnt++];
+}
 
-public:
-	/* 이중연결리스트 생성 */
-	HeadNode* createList() {
-		HeadNode* H = new HeadNode;
-		H->Lhead = NULL;
-		H->Rhead = NULL;
-		return H;
+void init()
+{
+	nodeCnt = 0;
+	head = getNode(-1);
+	tail = getNode(-1);
+	head->next = tail;
+	tail->prev = head;
+}
+
+void addNode2Head(int data)
+{
+	Node* newNode = getNode(data);
+
+	newNode->next = head->next;
+	head->next->prev = newNode;
+	newNode->prev = head;
+	head->next = newNode;
+}
+
+void addNode2Tail(int data)
+{
+	Node* newNode = getNode(data);
+	newNode->next = tail;
+	newNode->prev = tail->prev;
+	tail->prev->next = newNode;
+	tail->prev = newNode;
+}
+
+void addNode2Num(int data, int num)
+{
+	Node* ptr = head;
+
+	int idx = 1;
+	Node* newNode = getNode(data);
+	if (num == 1) {
+		newNode->next = head->next;
+		head->next->prev = newNode;
+		newNode->prev = head;
+		head->next = newNode;
+
 	}
-
-	/* 이중연결리스트 끝에 노드 삽입*/
-	void addEndNode(HeadNode* H, int adddata) {
-		Node* prevNode;
-		Node* newNode = new Node;
-		newNode->data = adddata;
-		newNode->Llink = NULL;
-		newNode->Rlink = NULL;
-
-		if (H->Rhead == NULL) { // 리스트가 비어있을 때
-			H->Rhead = newNode;
-			H->Lhead = newNode;
-		}
-		else { // 리스트에 노드가 있을 때
-			prevNode = H->Lhead;  // 리스트의 마지막노드는 Lhead가 가리키고 있을 것이므로 이걸 prevNode로 
-			prevNode->Rlink = newNode;
-			newNode->Llink = prevNode;
-			H->Lhead = newNode; // 새 노드가 마지막 노드가 된다.
-		}
-	}
-
-	/* 이중연결리스트 특정 노드 삽입*/
-	void addThisNode(HeadNode* H, int afterthisdata, int adddata) {
-		Node* prevNode;
-		Node* newNode = new Node;
-		newNode->data = adddata;
-		newNode->Llink = NULL;
-		newNode->Rlink = NULL;
-
-		prevNode = H->Rhead;
-
-		while (prevNode->data != afterthisdata) {
-			prevNode = prevNode->Rlink;
-		}
-
-		newNode->Rlink = prevNode->Rlink; //새 노드가 이전노드가 가리키던 노드를 가리키게
-		newNode->Llink = prevNode;  // 새노드의 Llink가 이전노드를 가리키게
-		prevNode->Rlink = newNode;  // 이전노드의 Rlink가 새노드를 가리키게
-		newNode->Rlink->Llink = newNode; // 새노드의 다음 노드의 Llink가 이전노드가 아닌 새노드를 가리키게
-	}
-
-	/* 이중연결리스트 특정 노드 삭제*/
-	void deleteNode(HeadNode* H, int deldata) {
-		Node* delNode;
-		delNode = H->Rhead;
-
-		if (delNode == NULL) return; // 리스트가 빈 경우
-
-		while (delNode->data != deldata) {
-			delNode = delNode->Rlink;
-		}
-
-		if (delNode == H->Lhead) { //삭제할 노드가 리스트의 마지막 노드일 때
-			if (delNode == H->Rhead) { // 삭제할 노드가 유일한 노드일 때 
-				H->Rhead = NULL;
-				H->Lhead = NULL;
+	else {
+		while (1) {
+			if (idx == num) {
+				newNode->next = ptr->next;
+				newNode->prev = ptr;
+				ptr->next->prev = newNode;
+				ptr->next = newNode;
+				break;
 			}
-			else {
-				delNode->Llink->Rlink = NULL; //삭제할노드 바로 앞 노드의 Rlink가 NULL
-				H->Lhead = delNode->Llink;  // 헤드의 Lhead가 삭제할노드 바로 앞 노드를 가리키게
-			}
+			ptr = ptr->next;
+			idx++;
 		}
-		else if (delNode == H->Rhead) { //삭제할 노드가 리스트의 첫 노드일 때
-			H->Rhead = delNode->Rlink;
-			delNode->Rlink->Llink = NULL;
-		}
-		else {
-			delNode->Llink->Rlink = delNode->Rlink;
-			delNode->Rlink->Llink = delNode->Llink;
-		}
-		delete delNode;
-		cout << deldata << "를 가지는 노드를 삭제했습니다." << endl;
 	}
+}
 
-	/* 이중연결리스트 출력*/
-	void printList(HeadNode* L) {
-
-		//노드 순서대로 리스트 출력
-		Node* p;
-
-		p = L->Rhead;
-
-		if (p == NULL) { // 리스트가 빈 경우
-			cout << endl << "연결 리스트가 비어있습니다." << endl << endl;
-			return;
-		}
-
-		cout << "연결리스트 목록 = ( ";
-
-		while (p != NULL) {
-			cout << p->data;
-			p = p->Rlink;
-			if (p != NULL) cout << " --> ";
-		}
-		cout << " )" << endl << endl;
+int findNode(int data)
+{
+	Node* ptr = head->next;
+	int cnt = 1;
+	while (ptr != tail) {
+		if (ptr->data == data)
+			return cnt;
+		cnt++;
+		ptr = ptr->next;
 	}
+	return cnt;
+}
 
-};
+void removeNode(int data)
+{
+	Node* ptr = head->next;
+	while (ptr != tail) {
+		if (ptr->data == data) {
+			break;
+		}
+		ptr = ptr->next;
+	}
+	if (ptr == tail) return;
+
+	if (ptr->prev == head) {
+		head->next = ptr->next;
+		ptr->next->prev = head;
+	}
+	else if (ptr->next == tail) {
+		tail->prev = ptr->prev;
+		ptr->prev->next = tail;
+	}
+	else {
+		ptr->prev->next = ptr->next;
+		ptr->next->prev = ptr->prev;
+	}
+}
+
+int getList(int output[MAX_NODE])
+{
+	Node* ptr = head->next;
+	int cnt = 0;
+	while (ptr != tail) {
+		output[cnt++] = ptr->data;
+		ptr = ptr->next;
+	}
+	return cnt;
+}
+
+int getReversedList(int output[MAX_NODE])
+{
+	Node* ptr = tail->prev;
+	int cnt = 0;
+
+	while (ptr != head) {
+		output[cnt++] = ptr->data;
+		ptr = ptr->prev;
+	}
+	return cnt;
+}
